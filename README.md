d554b5b600f396a5af4df3d7c4f1c650f408baac
John Hult <johnhult89@gmail.com>
2015-02-15 19:45:38 +0100

    base for store

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # store
+Run the following in terminal to get going:
+
+###Step 1:
+`cd "path/to/project"`
+//To get to the root folder of the project. Change `"path/to/project"` to the actual path.
+
+###Step 2:
+`npm install`
+// Do this to install all dependencies from node_modules.
+//Prerequisite: Have [node.js and npm installed](https://docs.npmjs.com/getting-started/installing-node) on computer.
+
+###Step 3:
+`grunt`
+//Type this stuff in and the magic begins. This will load a page on http://localhost:8000 with livereload.
---
+++ gruntfile.js
@@ -0,0 +1,64 @@
+'use strict';
+module.exports = function(grunt) {
+    
+    require('load-grunt-tasks')(grunt);
+    
+    grunt.initConfig({
+
+        
+        pkg: grunt.file.readJSON('package.json'),
+
+        sass: {
+            build: {
+                files: {
+                    'build/css/main.css': 'dev/styles/main.scss'
+                }
+            }
+        },
+
+        watch: {
+            html: {
+                files: ['index.html'],
+                options: {
+                    livereload: true
+                }
+            },
+            js: {
+                files: ['dev/scripts/*.js'],
+                tasks: ['uglify'],
+                options: {
+                    livereload: true
+                }
+            },
+            scss: {
+                files: ['dev/styles/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+
+        uglify: {
+            build: {
+                files: {
+                    'build/scripts/main.min.js': ['dev/scripts/main.js']
+                }
+            }
+        },
+
+        connect: {
+            server: {
+                options: {
+                    port: 8000,
+                    base: './',
+                    livereload: true,
+                    open: 'http://localhost:8000'
+                }
+            }
+        }
+
+    });
+
+    grunt.registerTask('default', ['sass', 'uglify', 'connect', 'watch']);
+};

e4efcbd9df476999e0f03361aabfb428c4d60ebd
John Hult <johnhult89@gmail.com>
2015-02-23 08:19:37 +0100

    script for navigation

---
+++ dev/scripts/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$('.reklamation').on('click', function() {
+		location = 'dev/html/reklamation-start.html';
+	});	
+	$('.App-header--navigation-left').on('click', function() {
+		location ='../../index.html';
+	});	
+});

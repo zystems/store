7771726c928027cc5ec5f48c1c50b2beb2dcd115
John Hult <johnhult89@gmail.com>
2015-02-23 10:39:16 +0100

    Navigation complete

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -3,6 +3,34 @@ $(document).ready(function() {
 		location = 'dev/html/reklamation-start.html';
 	});	
 	$('.App-header--navigation-left').on('click', function() {
-		location ='../../index.html';
-	});	
+		switch (location.pathname) {
+			case menuItems[1].url :
+				location = '../../'+menuItems[0].url;
+				break;
+			case menuItems[2].url :
+				location = menuItems[1].url;
+				break;
+		}
+	});
+	$('.App-footer--navigation-right').on('click', function() {
+		switch (location.pathname) {
+			case menuItems[1].url :
+				location = menuItems[2].url;
+				break;
+		}
+	});
 });
+
+var menuItems = [
+	{
+        name : 'Main',  
+        url : 'index.html'  
+    },  
+    {
+        name : 'Reklamation',
+        url : '/dev/html/reklamation-start.html'  
+    },
+    {
+        name : 'Reklamation-confirm',  
+        url : '/dev/html/reklamation-confirm.html'
+    }]
